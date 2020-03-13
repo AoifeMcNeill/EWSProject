2e886d9963af57c59ce95b794ebd0f7b904ca6fc
AoifeMcNeill <amcneill849@gmail.com>
2020-03-13 06:28:40 +0000

    set up of basic express server.js file

---
+++ scripts/server.js
@@ -0,0 +1,17 @@
+//load the package and create our app
+var express = require('express');
+var app = express();
+const PORT = process.env.PORT || 8080;
+
+//Set the port based on environment
+var port = PORT;
+
+//send our index.html file to the user for the home page
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
+
+//Start the server
+app.listen(PORT);
+
+console.log("Express Server running at http://127.0.0.1:'.PORT");
